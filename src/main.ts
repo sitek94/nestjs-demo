bcef1c10f17c5444c4eddc5bd071abf800defcf3
Maciek <msitkowski94@gmail.com>
2022-02-28 20:25:59 +0100

    add basic validation of dto in "/signup" route

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
+import { ValidationPipe } from '@nestjs/common'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.useGlobalPipes(
+    new ValidationPipe({
+      // Filter out properties that should not be received by the method handler
+      whitelist: true,
+    }),
+  )
   await app.listen(3333)
 }
 bootstrap()
